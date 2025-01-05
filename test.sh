f87d55f8405a6495406e860ac4a8d1eaca4a37c9
david <dasmith@zero-to-prod.com>
2025-01-05 06:14:45 -0500

    maint/php84 (#5)
    * maint/php84 Updated `.github/workflows/test.yml`.
    
    * maint/php84 Updated `.gitignore`.
    
    * maint/php84 Updated `SECURITY.md`.
    
    * maint/php84 Updated `composer.json`.
    
    * maint/php84 Updated `docker-compose.yml`.
    
    * maint/php84 Created `docker/php84/Dockerfile`.
    
    * maint/php84 Created `docker/php84/php.ini`.
    
    * maint/php84 Updated `test.sh`.
    
    ---------
    
    Co-authored-by: david_smith <david_smith@sweetwater.com>

--- test.sh
+++ test.sh
@@ -1,21 +1,12 @@
-# run-tests.sh
+#!/bin/bash
 set -e
 
-php_versions=("php83" "php82" "php81")
+php_versions=("php84" "php83" "php82" "php81")
 
 for version in "${php_versions[@]}"; do
-  if [ -f composer.lock ]; then
-    echo "removing composer.lock"
-    rm composer.lock
-  fi
+  docker compose run --rm "${version}composer" composer update --no-cache
 
-  docker compose run --rm "$version"composer composer install
-
-  echo "Running tests on $version..."
   if ! docker compose run --rm "$version" vendor/bin/phpunit; then
-    echo "Tests failed on $version."
     exit 1
   fi
 done
-
-echo "All tests passed!"
